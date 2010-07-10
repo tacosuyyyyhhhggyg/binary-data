01769f7147437267acf6e7379d31e82dc885eac4
James S <i@nixeagle.org>
2010-07-10 21:56:06 +0000

    Tests for TYPE-READER-BODY

--- tests.lisp
+++ tests.lisp
@@ -16,4 +16,21 @@
   (is (equalp #(255 0) (out->seq s (binary-data:write-little-endian-integer 255 s 2))))
   (is (equalp #(0 1) (out->seq s (binary-data:write-little-endian-integer 256 s 2)))))
 
+(test (type-reader-body :suite :binary-data-tests)
+  "Tests what macros would be passing this to validate function behavior.
+
+This breaking means every macro using it will also break."
+  (is (equalp `(binary-data::read-value 'some-type ,*standard-output*)
+       (binary-data::type-reader-body '(some-type) *standard-output*)))
+  ;; Testing parsing of the (:reader . (....) jazz. The way pcl wrote this
+  ;; code is to depend on the length of the list, as it expects :reader,
+  ;; :writer to both be present.
+  (is (equalp `(let ((var ,*standard-output*)) var)
+              (binary-data::type-reader-body '((:reader . ((var) var)) ())
+                                             *standard-output*))))
+
+
+
+
+
 ;;; END

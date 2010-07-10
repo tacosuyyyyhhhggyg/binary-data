402ab143a93f2884068187131fad48fbbbaef7c9
James S <i@nixeagle.org>
2010-07-10 02:58:16 +0000

    Testcase for WRITE-LITTLE-ENDIAN-INTEGER

--- tests.lisp
+++ tests.lisp
@@ -3,7 +3,17 @@
 
 (in-package :binary-data-tests)
 
-(defmacro out->seq (&body body)
+(defmacro out->seq (stream &body body)
   "All output of BODY goes to a vector."
-  `(with-output-to-sequence (*standard-output*)
+  `(with-output-to-sequence (,stream)
      ,@body))
+
+(in-suite* :binary-data-tests)
+
+(test (write-little-endian-integer :suite :binary-data-tests)
+  "Little endian integer written out for storage on a harddrive."
+  (is (equalp #(10) (out->seq s (binary-data:write-little-endian-integer 10 s))))
+  (is (equalp #(255 0) (out->seq s (binary-data:write-little-endian-integer 255 s 2))))
+  (is (equalp #(0 1) (out->seq s (binary-data:write-little-endian-integer 256 s 2)))))
+
+;;; END
